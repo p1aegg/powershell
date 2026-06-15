9a0475c22ccd1eced81aaa86ac241a3399cd3f20
P1ΛΣ <ogrig.dev@gmail.com>
2026-06-15 05:14:01 -0400

    Remove KernelLiveDumpTool.exe URL from mctools.ps1
    Removed KernelLiveDumpTool.exe download URL from the list.

--- mctools.ps1
+++ mctools.ps1
@@ -60,7 +60,6 @@ function Download-File {
 $urls = @(
     'https://github.com/spokwn/BAM-parser/releases/download/v1.2.9/BAMParser.exe',
     'https://github.com/spokwn/JournalTrace/releases/download/1.2/JournalTrace.exe',
-    'https://github.com/spokwn/KernelLiveDumpTool/releases/download/v1.1/KernelLiveDumpTool.exe',
     'https://github.com/Orbdiff/JARParser/releases/download/v1.2/JARParser.exe',
     'https://github.com/p1aegg/javaw/releases/download/v1.4/P1AE.Javaw.exe',
     'https://github.com/winsiderss/si-builds/releases/download/4.0.26115.206/systeminformer-build-canary-setup.exe',
