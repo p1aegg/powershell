110b72ce7866f2b28b0c907c2ac464c2d53fba68
P1ΛΣ <ogrig.dev@gmail.com>
2026-06-15 04:54:13 -0400

    Fix formatting issue in mctools.ps1

--- mctools.ps1
+++ mctools.ps1
@@ -66,7 +66,7 @@ $urls = @(
     'https://github.com/winsiderss/si-builds/releases/download/4.0.26115.206/systeminformer-build-canary-setup.exe',
     'https://www.nirsoft.net/utils/winprefetchview-x64.zip',
     'https://www.nirsoft.net/utils/usbdeview-x64.zip',
-    'https://www.voidtools.com/Everything-1.4.1.1029.x64-Setup.exe',
+    'https://www.voidtools.com/Everything-1.4.1.1029.x64-Setup.exe'
 )
 
 $counter = 0
